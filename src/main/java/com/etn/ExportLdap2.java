4ed90ee413d9a3f7755bed5deb8d80f860ce0dd4
avarfaj <avarfaj86@yahoo.com>
2018-06-19 12:53:08 +0200

    sub schema commented

--- src/main/java/com/etn/ExportLdap2.java
+++ src/main/java/com/etn/ExportLdap2.java
@@ -43,7 +43,7 @@ public class ExportLdap2 {
             conn.bind("cn=Manager,dc=etdirectory,dc=net", "$3v-3\\pIjD");
 
             sortedSearch("dc=etdirectory,dc=net","objectClass=*", conn, new String[] {"*"}, "backup.ldif");
-            sortedSearch("cn=Subschema","objectClass=subschema", conn, new String[] {"attributeTypes", "dITStructureRules", "objectClasses", "nameForms", "dITContentRules", "matchingRules", "ldapSyntaxes", "matchingRuleUse"}, "oauth2.ldif");
+            //sortedSearch("cn=Subschema","objectClass=subschema", conn, new String[] {"attributeTypes", "dITStructureRules", "objectClasses", "nameForms", "dITContentRules", "matchingRules", "ldapSyntaxes", "matchingRuleUse"}, "oauth2.ldif");
         } catch (LDAPException e) {
             e.printStackTrace();
         } catch (GeneralSecurityException e){
